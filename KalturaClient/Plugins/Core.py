5bc9f0dcac6c6bbce4c6e264de5116c5f5cf65ef
Jenkins <jenkins@kaltura.com>
2018-04-05 06:47:28 +0000

    Auto-generated by Jenkins

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -29,9 +29,20 @@
 # @subpackage Client
 from __future__ import absolute_import
 
-from ..Base import *
+from ..Base import (
+    getXmlNodeBool,
+    getXmlNodeFloat,
+    getXmlNodeInt,
+    getXmlNodeText,
+    KalturaClientPlugin,
+    KalturaEnumsFactory,
+    KalturaObjectBase,
+    KalturaObjectFactory,
+    KalturaParams,
+    KalturaServiceBase,
+)
 
-API_VERSION = '4.7.43.16223'
+API_VERSION = '4.7.43.12166'
 
 ########## enums ##########
 # @package Kaltura
