c4be08d80912f29e2ea833898867f89f698f996d
Jenkins <jenkins@kaltura.com>
2018-08-27 11:08:49 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-18-08-26',
+            'clientTag': 'python-18-08-27',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
