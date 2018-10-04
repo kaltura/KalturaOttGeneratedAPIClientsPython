813a120264d4bea058147dd9b5a7756460fcbf25
Jenkins <jenkins@kaltura.com>
2018-10-04 14:09:44 +0000

    Auto-generated by Jenkins

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KalturaOttApiClient',
-    version='5.0.1.22613',
+    version='5.0.1.25434',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
