4e8b9ef9f59a6c08b4fce74bcec129d13dc194e4
Jenkins <jenkins@kaltura.com>
2018-10-14 23:21:02 +0000

    Auto-generated by Jenkins

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.0.3.14284'
+API_VERSION = '5.0.3.41999'
 
 ########## enums ##########
 # @package Kaltura
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KalturaOttApiClient',
-    version='5.0.3.14284',
+    version='5.0.3.41999',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
