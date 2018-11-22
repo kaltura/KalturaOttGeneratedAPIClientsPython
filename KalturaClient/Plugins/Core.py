eefed7013d07ef6dfd57ad1b0a082f80ae790877
Jenkins <jenkins@kaltura.com>
2018-11-22 08:52:09 +0000

    Auto-generated by Jenkins

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.0.1.16994'
+API_VERSION = '5.0.1.15905'
 
 ########## enums ##########
 # @package Kaltura
@@ -25649,7 +25649,6 @@ class KalturaHousehold(KalturaObjectBase):
 
         # Household external identifier
         # @var string
-        # @insertonly
         self.externalId = externalId
 
         # Household limitation module identifier
@@ -27302,7 +27301,8 @@ class KalturaAssetService(KalturaServiceBase):
         KalturaServiceBase.__init__(self, client)
 
     def add(self, asset):
-        """Add a new asset"""
+        """Add a new asset.
+                    For metas of type bool-&gt; use kalturaBoolValue, type number-&gt; KalturaDoubleValue, type date -&gt; KalturaLongValue, type string -&gt; KalturaStringValue"""
 
         kparams = KalturaParams()
         kparams.addObjectIfDefined("asset", asset)
@@ -27399,7 +27399,8 @@ class KalturaAssetService(KalturaServiceBase):
         return getXmlNodeBool(resultNode)
 
     def update(self, id, asset):
-        """update an existing asset"""
+        """update an existing asset.
+                    For metas of type bool-&gt; use kalturaBoolValue, type number-&gt; KalturaDoubleValue, type date -&gt; KalturaLongValue, type string -&gt; KalturaStringValue"""
 
         kparams = KalturaParams()
         kparams.addIntIfDefined("id", id);
