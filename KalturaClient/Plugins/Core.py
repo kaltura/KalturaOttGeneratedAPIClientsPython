0b1b85406d1bc48ecd3b16a3de9b58be0a42ee79
Jenkins <jenkins@kaltura.com>
2019-01-08 09:17:05 +0000

    Auto-generated by Jenkins

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -8,7 +8,7 @@
 # to do with audio, video, and animation what Wiki platfroms allow them to do with
 # text.
 #
-# Copyright (C) 2006-2019  Kaltura Inc.
+# Copyright (C) 2006-2018  Kaltura Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.1.1.42819'
+API_VERSION = '5.1.1.16624'
 
 ########## enums ##########
 # @package Kaltura
