9daff67e83a238b8727fa8e8d054b7e5e219fc52
Backend CI <ott.rnd.core@kaltura.com>
2019-03-27 00:10:13 +0000

    Auto-generated by Jenkins

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='KalturaOttApiClient',
-    version='5.2.0.43177',
+    version='5.2.0.43114',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
