d2a683c157b5055963e3fae3ee18413a3e972d74
Backend CI <ott.rnd.core@kaltura.com>
2019-09-11 10:54:45 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-19-08-30',
+            'clientTag': 'python-19-09-11',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
