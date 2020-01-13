ee0c28ee0d479d7184baec741beafc4affd44e00
Backend CI <ott.rnd.core@kaltura.com>
2020-01-13 17:24:04 +0000

    Auto-generated by Jenkins

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.3.0.14313'
+API_VERSION = '5.3.0.14314'
 
 ########## enums ##########
 # @package Kaltura
@@ -39656,11 +39656,11 @@ class KalturaSystemService(KalturaServiceBase):
     def __init__(self, client = None):
         KalturaServiceBase.__init__(self, client)
 
-    def clearLocalServerCache(self, action = NotImplemented, key = NotImplemented):
+    def clearLocalServerCache(self, clearCacheAction = NotImplemented, key = NotImplemented):
         """Clear local server cache"""
 
         kparams = KalturaParams()
-        kparams.addStringIfDefined("action", action)
+        kparams.addStringIfDefined("clearCacheAction", clearCacheAction)
         kparams.addStringIfDefined("key", key)
         self.client.queueServiceActionCall("system", "clearLocalServerCache", "None", kparams)
         if self.client.isMultiRequest():
