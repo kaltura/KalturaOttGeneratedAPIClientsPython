878cb0979c8ea4836dda69bf1fdf5dc25d0aece7
Backend CI <ott.rnd.core@kaltura.com>
2020-02-09 11:01:55 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-20-02-04',
+            'clientTag': 'python-20-02-09',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.2.8.14082'
+API_VERSION = '5.2.8.14085'
 
 ########## enums ##########
 # @package Kaltura
