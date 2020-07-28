d1298ebb2bac0b77da984a46f6f2b8375154523a
Backend CI <ott.rnd.core@kaltura.com>
2020-07-28 03:21:21 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/124, branch 5_4_1

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '5.4.0.28215'
+API_VERSION = '5.4.0.28218'
 
 ########## enums ##########
 # @package Kaltura
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.txt')) as f:
 
 setup(
     name='KalturaOttApiClient',
-    version='5.4.0.28215',
+    version='5.4.0.28218',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
