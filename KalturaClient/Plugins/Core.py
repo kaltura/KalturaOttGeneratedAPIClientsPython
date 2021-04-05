f6b095c158815eeacbe2289192f801b4647afe96
Backend CI <ott.rnd.core@kaltura.com>
2021-04-05 20:47:49 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/419, branch 6_2_2

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -5,7 +5,7 @@
 #                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
 #
 # This file is part of the Kaltura Collaborative Media Suite which allows users
-# to do with audio, video, and animation what Wiki platforms allow them to do with
+# to do with audio, video, and animation what Wiki platfroms allow them to do with
 # text.
 #
 # Copyright (C) 2006-2021  Kaltura Inc.
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '6.2.0.28994'
+API_VERSION = '6.2.0.28995'
 
 ########## enums ##########
 # @package Kaltura
