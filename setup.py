01b810b3694030c195031ae618ba4b6142aeaf4b
Backend CI <ott.rnd.core@kaltura.com>
2021-05-10 07:58:50 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/454, branch priceDetails+partnerAdd

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.txt')) as f:
 
 setup(
     name='KalturaOttApiClient',
-    version='6.3.0.29047',
+    version='6.4.0.29295',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
