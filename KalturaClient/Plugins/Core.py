797442bee8464894f587cd31c1fd78d4f67768a8
Backend CI <ott.rnd.core@kaltura.com>
2021-06-21 12:52:29 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/512, branch 660_BEO-9899_Series_recording_options

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -5,7 +5,7 @@
 #                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
 #
 # This file is part of the Kaltura Collaborative Media Suite which allows users
-# to do with audio, video, and animation what Wiki platforms allow them to do with
+# to do with audio, video, and animation what Wiki platfroms allow them to do with
 # text.
 #
 # Copyright (C) 2006-2021  Kaltura Inc.
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '6.5.0.29186'
+API_VERSION = '6.5.0.29187'
 
 ########## enums ##########
 # @package Kaltura
