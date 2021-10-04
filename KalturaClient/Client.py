e7a5c8805655fddaf962ea895d543d9585d52fe3
Backend CI <ott.rnd.core@kaltura.com>
2021-10-04 10:32:04 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/652, branch 6_8_1

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-21-09-30',
+            'clientTag': 'python-21-10-04',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '6.8.0.29308'
+API_VERSION = '6.8.0.29309'
 
 ########## enums ##########
 # @package Kaltura
