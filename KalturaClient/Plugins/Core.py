8a59a86a6610932a7490819f736411744948f0e0
Backend CI <ott.rnd.core@kaltura.com>
2022-02-10 15:59:18 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/785, branch 7_2_1

--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -8,7 +8,7 @@
 # to do with audio, video, and animation what Wiki platforms allow them to do with
 # text.
 #
-# Copyright (C) 2006-2021  Kaltura Inc.
+# Copyright (C) 2006-2022  Kaltura Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '7.2.0.29747'
+API_VERSION = '7.2.0.29748'
 
 ########## enums ##########
 # @package Kaltura
