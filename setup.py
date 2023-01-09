2f044508afa42c8499e8c445f470e04546e75875
Backend CI <ott.rnd.core@kaltura.com>
2023-01-09 11:23:59 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1027, branch BEO-13063_add_recording

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.txt')) as f:
 
 setup(
     name='KalturaOttApiClient',
-    version='8.3.1.30231',
+    version='8.4.3.30333',
     url='https://github.com/kaltura/KalturaOttGeneratedAPIClientsPython',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
     install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
