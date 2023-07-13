08b5dda4dfad4f3a3fb706c9b8127883036e5c0d
Backend CI <ott.rnd.core@kaltura.com>
2023-07-13 08:13:51 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1162, branch 8_0_0

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -143,7 +143,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-22-09-08',
+            'clientTag': 'python-23-07-13',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
--- KalturaClient/Plugins/Core.py
+++ KalturaClient/Plugins/Core.py
@@ -8,7 +8,7 @@
 # to do with audio, video, and animation what Wiki platforms allow them to do with
 # text.
 #
-# Copyright (C) 2006-2022  Kaltura Inc.
+# Copyright (C) 2006-2023  Kaltura Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -42,7 +42,7 @@ from ..Base import (
     KalturaServiceBase,
 )
 
-API_VERSION = '8.0.0.30012'
+API_VERSION = '8.0.0.30013'
 
 ########## enums ##########
 # @package Kaltura
@@ -45853,7 +45853,7 @@ class KalturaIotClientConfiguration(KalturaObjectBase):
         self.password = password
 
         # topics
-        # @var array of KalturaStringValue
+        # @var string
         self.topics = topics
 
         # status
@@ -45877,7 +45877,7 @@ class KalturaIotClientConfiguration(KalturaObjectBase):
         'thingId': getXmlNodeText, 
         'username': getXmlNodeText, 
         'password': getXmlNodeText, 
-        'topics': (KalturaObjectFactory.createArray, 'KalturaStringValue'), 
+        'topics': getXmlNodeText, 
         'status': getXmlNodeText, 
         'message': getXmlNodeText, 
     }
@@ -45900,7 +45900,7 @@ class KalturaIotClientConfiguration(KalturaObjectBase):
         kparams.addStringIfDefined("thingId", self.thingId)
         kparams.addStringIfDefined("username", self.username)
         kparams.addStringIfDefined("password", self.password)
-        kparams.addArrayIfDefined("topics", self.topics)
+        kparams.addStringIfDefined("topics", self.topics)
         kparams.addStringIfDefined("status", self.status)
         kparams.addStringIfDefined("message", self.message)
         return kparams
