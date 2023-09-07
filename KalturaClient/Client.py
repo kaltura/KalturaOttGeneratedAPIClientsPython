fa411d3d9e39ddf907dd1a084dcbd0e2147b6fb5
Backend CI <ott.rnd.core@kaltura.com>
2023-09-07 08:01:31 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1205, branch BEO-14481-1

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -143,7 +143,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-23-09-06',
+            'clientTag': 'python-23-09-07',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
