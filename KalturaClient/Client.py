c2dfbff214018eb553c2c7cd7449805ef85aca18
Backend CI <ott.rnd.core@kaltura.com>
2023-09-22 11:02:18 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1212, branch BEO-14481-programasset-externalofferids-nullable

--- KalturaClient/Client.py
+++ KalturaClient/Client.py
@@ -143,7 +143,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-23-09-06',
+            'clientTag': 'python-23-09-22',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
